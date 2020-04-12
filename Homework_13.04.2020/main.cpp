cf5f333a9597902e894b4aafd156f65b5e47324e
Ershov-Artem <63228781+Ershov-Artem@users.noreply.github.com>
2020-04-12 15:45:54 +0300

    Add files via upload

---
+++ Homework_13.04.2020/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int dp(int a, int b){
+    if (a == 1 && b == 1)
+        return 1;
+    if (a < 0 || b < 0)
+        return 0;
+    return (dp(a - 1, b - 2) + dp(a - 2, b - 1));
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << dp(n, m);
+}
